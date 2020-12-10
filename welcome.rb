ec69b76b397d021e39b1986fa0621bc9a69394ea
jmartinmcg <gcas022015@coderacademy.edu.au>
2020-12-11 10:53:09 +1100

    added welcome options

---
+++ welcome.rb
@@ -0,0 +1,12 @@
+# Welcome Page for the fortune telling App
+class Welcome
+    # shows the options available within the App
+    def show_options
+        puts "\nWelcome to the Oracle!" 
+        puts "Do you come with a specific burning question that you want a 'yes' or 'no' answer to? Or do you have a more broad area of life where you want to receive guidance?"
+        puts "Type 1 for Yes or No Qu"
+        puts "Type 2 for broad guidance"
+        puts "Type 3 for a mystery oracle reading"
+        puts "Type 4 to Exit"
+    end
+end 
